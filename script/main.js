ea9db6b5125b4012373097cc0419938f2dcfde84
kittithatj <92021493+kittithatj@users.noreply.github.com>
2022-02-21 19:01:57 +0700

    storage bar

--- script/main.js
+++ script/main.js
@@ -1,6 +1,34 @@
-let bit = 0;
+//elements
+let bitDisplay = document.getElementById('bit-display');
+let storageBar = document.getElementById('storage-bar');
+let maxStorageDisplay = document.getElementById('max-storage');
+let dsTimelineModal = document.getElementById('ds-timeline');
 
+//game variable
+let bit = 0;
+let storage = 64;
+
+//init
+bitDisplay.innerHTML = `Bit : ${bit}`;
+maxStorageDisplay.innerHTML = `${storage} Bit`
+storageBar.innerHTML = `${storage} Bit`
+storageBar.max = `${storage}`
+
+//function
 function clickBit(){
-    bit++;
-    document.getElementById('bit-display').innerHTML = `Bit : ${bit}`
+    if(bit < storage && bit >= 0){
+        bit++;
+        bitDisplay.innerHTML = `Bit : ${bit}`
+        storageBar.value = `${bit}`;
+    }else{
+        bitDisplay.style = 'color: red;';
+    }
+}
+
+function openDsTimeline(){
+    dsTimelineModal.style = 'display: block;'
+}
+
+function closeModal(){
+    dsTimelineModal.style = 'display: none;'
 }
