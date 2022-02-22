bb08b6830c3bbf9ac6c8ec8f7a9fe8e452324322
kittithatj <92021493+kittithatj@users.noreply.github.com>
2022-02-22 17:41:05 +0700

    Storage Upgrade

--- script/main.js
+++ script/main.js
@@ -1,34 +1,78 @@
 //elements
-let bitDisplay = document.getElementById('bit-display');
-let storageBar = document.getElementById('storage-bar');
-let maxStorageDisplay = document.getElementById('max-storage');
-let dsTimelineModal = document.getElementById('ds-timeline');
+let BitDisplay = document.getElementById('bit-display');
+let StorageBar = document.getElementById('storage-bar');
+let MaxStorageDisplay = document.getElementById('max-storage');
+
+let PunchCardAmount = document.getElementById('punch-card-am');
+let PunchCardCost = document.getElementById('punch-card-cost');
+
+let MntDrumAmount = document.getElementById('mnt-drum-am');
+let MntDrumCost = document.getElementById('mnt-drum-cost');
 
 //game variable
 let bit = 0;
+let bitPerClick = 8;
 let storage = 64;
+let punchCard = {
+    storage : 800,
+    amount : 0,
+    cost : 64
+}
+let mntDrum = {
+    storage : 5000,
+    amount : 0,
+    cost : 4096
+}
 
 //init
-bitDisplay.innerHTML = `Bit : ${bit}`;
-maxStorageDisplay.innerHTML = `${storage} Bit`
-storageBar.innerHTML = `${storage} Bit`
-storageBar.max = `${storage}`
+bitUpdate();
+storageUpdate();
 
 //function
+function bitUpdate(){
+    BitDisplay.style = 'color: azure;';
+    BitDisplay.innerHTML = `Bit : ${bit}`;
+    StorageBar.value = `${bit}`;
+    StorageBar.max = `${storage}`;
+    MaxStorageDisplay.innerHTML = `Max ${storage} Bit`;
+}
+
+function storageUpdate(){
+    PunchCardAmount.innerHTML = `${punchCard.amount}`
+    PunchCardCost.innerHTML = `Cost : ${punchCard.cost} Bit`
+
+    MntDrumAmount.innerHTML = `${mntDrum.amount}`
+    MntDrumCost.innerHTML = `Cost : ${mntDrum.cost} Bit`
+
+}
+
 function clickBit(){
     if(bit < storage && bit >= 0){
-        bit++;
-        bitDisplay.innerHTML = `Bit : ${bit}`
-        storageBar.value = `${bit}`;
+        bit += bitPerClick;
+        bitUpdate();
     }else{
-        bitDisplay.style = 'color: red;';
+        BitDisplay.style = 'color: red;';
     }
 }
 
-function openDsTimeline(){
-    dsTimelineModal.style = 'display: block;'
+function punchCardUpgrade() {
+    if(bit>=punchCard.cost){
+        bit -= punchCard.cost;
+        storage += punchCard.storage;
+        bitUpdate();
+        punchCard.amount++;
+        punchCard.cost += Math.round(punchCard.cost);
+        storageUpdate();
+    }
 }
 
-function closeModal(){
-    dsTimelineModal.style = 'display: none;'
+function mntDrumUpgrade() {
+    if(bit>=mntDrum.cost){
+        bit -= mntDrum.cost;
+        storage += mntDrum.storage;
+        bitUpdate();
+        mntDrum.amount++;
+        mntDrum.cost += Math.round(mntDrum.cost);
+        storageUpdate();
+    }
 }
